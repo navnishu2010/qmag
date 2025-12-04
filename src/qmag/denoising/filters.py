04b4f957f63b4960b54f69d34b10b3a7aa33043f
naveen <naveen@Gomugomuno.local>
2025-12-04 15:48:52 +0530

    Added class for notch filter

--- src/qmag/denoising/filters.py
+++ src/qmag/denoising/filters.py
@@ -1,3 +1,16 @@
 import math 
 import numpy as np
 import scipy
+from scipy import signal
+
+class NotchFilter(BaseFilter):
+    def __init__(self, freq: float, fs: float, q: float = 30.0):
+        self.freq = freq
+        self.fs = fs
+        self.q = q
+        nyquist = 0.5 * fs
+        normal_freq = freq / nyquist
+        self.b, self.a = signal.iirnotch(normal_freq, q)
+
+    def filter(self, data: np.ndarray) -> np.ndarray:
+        return signal.filtfilt(self.b, self.a, data)
